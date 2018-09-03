a0ce9ef9790a2abf18eab932e29ab6a13ee65feb
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-09-03 15:04:09 -0400

    Added first test

---
+++ src/flash/flash.component.spec.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { FlashComponent } from './flash.component';
+
+describe("Flash", () => {
+  describe("with the default state", () => {
+    it("renders without crashing", () => {
+      const wrapper = mount(
+        <FlashComponent
+          open={false}
+          closeFlash={() => {}}
+          message={""}
+        />
+      );
+      expect(wrapper).toBeDefined();
+    });
+  });
+});
